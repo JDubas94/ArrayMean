0fcbeb041e86f2d7c812ecb45270b5d83505c975
Julia Dubas <uliadubas94@gmail.com>
2022-07-18 21:15:41 +0300

    Add project files.

---
+++ ArrayMean/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArray = new int[] { 8, 6, 10, 11, 9, 6, 10 };
+
+            int x = 0;
+
+            for(int i = 0; i < newArray.Length; i++)
+            {
+                x += newArray[i];
+            }
+            int rezult = x / newArray.Length;
+
+             Console.WriteLine(rezult);
+
+            Console.ReadLine();
+        }
+    }
+}
